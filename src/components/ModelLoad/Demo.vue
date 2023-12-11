f9c3fa9eabc92a7de3ace0237f5ab8a393abe7b9
musixnotmusic <279601306@qq.com>
2023-12-11 18:04:51 +0800

    ...

--- src/components/ModelLoad/Demo.vue
+++ src/components/ModelLoad/Demo.vue
@@ -4,21 +4,32 @@
 
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue';
+import mapboxgl from 'mapbox-gl';
 import MapboxGLInit from '../Map/MapboxGLInit.vue';
 import DemoModelLayer from './DemoModelLayer';
+import Test from './ModelDemo/Demo';
+
+window.mapboxgl = mapboxgl;
 
 const mapboxGLLoadedFunc = (map) => {
+    window.mapIns = map;
     console.log('mapboxGLLoadedFunc ==>', map)
     addDemoModelLayer(map);
 }
 
 const center = ref([104.1465432836781, 30.857102559661133]);
 let instance;
+let test;
 const addDemoModelLayer = (map) => {
-    if (!instance) {
-        instance = new DemoModelLayer('demo', map);
+    // if (!instance) {
+    //     instance = new DemoModelLayer('demo', map);
+    // }
+    // instance.render();
+
+    if (!test) {
+        test = new Test('test', map);
     }
-    instance.render();
+    test.render();
 }
 
 onMounted(() => {
@@ -26,6 +37,7 @@ onMounted(() => {
 
 onUnmounted(() => {
     if(instance) instance.dispose();
+    if(test) test.dispose();
 })
     
 </script>
---
+++ src/components/ModelLoad/ModelDemo/Demo.js
@@ -0,0 +1,148 @@
+import * as THREE from 'three';
+import { TransformControls } from 'three/addons/controls/TransformControls.js';
+
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import mapboxgl from "mapbox-gl";
+
+import { radarTestSet } from "../Constants";
+
+import BaseModelLayer from "./BaseModel";
+
+/***
+ * 矩形立方体的 等值面结构
+ */
+export default class Test extends BaseModelLayer{
+  constructor (id, map) {
+    super(id, map);
+    
+    this.modelSize = { x: 2000, y: 2000, z: 4000 }
+
+    this.type = 'radar'
+
+    this.resizeBind = this.resize.bind(this);
+
+    this.data = radarTestSet;
+    // 预计边界
+
+    this.zoomBind = this.zoom.bind(this);
+
+    this.control = new TransformControls( this.camera, this.renderer.domElement );
+
+    window.Test = this;
+
+  }
+
+  zoom() {
+    // let zoom = this.map.getZoom();
+    // if (this.css2DRenderer) {
+    //   const list = this.css2DRenderer.domElement.querySelectorAll('.name')
+    //   list.forEach(dom => {
+    //     dom.style.transform = `scale(${zoom / 10})`;
+    //   })
+    // }
+  }
+
+  addEventListener() {
+    if (this.map) {
+      this.map.on('zoom', this.zoomBind);
+    }
+  }
+
+  removeEventListener() {
+    if (this.map) {
+      this.map.off('zoom', this.zoomBind);
+    }
+  }
+
+  render () {
+    return this.initMesh().then(() => {
+      this.addLight();
+      this.drawLayer();
+      return null;
+    })
+  }
+
+  initMesh () {
+    const { scene } = this
+    const loader = new FBXLoader();
+
+
+    return new Promise((resolve, reject) => {
+      loader.load( '/model/fbx/radar2.fbx',  ( model ) => {
+        // loader.load( '/model/fbx/mig23mld.fbx',  ( model ) => {
+        console.log('this.modelSize ==>');
+        this.data.forEach((item, index) => {
+          const object = model.clone();
+
+          this.setMeshSide(object);
+
+          object.rotation.x += Math.PI / 2;
+
+          this.setObjectCenter(object);
+
+          const custom = new THREE.Object3D();
+
+          custom.name = item.name;
+
+          custom.userData = {
+            lon: item.lon,
+            lat: item.lat,
+            type: this.type,
+            scale: 100
+          }
+
+          custom.add(object)
+
+          this.setObjectCoords(custom);
+
+          const axesHelper = new THREE.AxesHelper(500);
+          custom.add(axesHelper);
+          
+          // scene.add( custom );
+          this.addNewScene( custom );
+
+          if (index === 0) {
+            const { control } = this;
+            control.addEventListener( 'dragging-changed', ( event ) => {
+
+              controls.enabled = ! event.value;
+    
+            } );
+            control.attach( custom );
+            scene.add( control );
+          }
+
+          this.addCSS2Object(custom, item.name);
+        })
+
+        resolve(model)
+      });
+    })
+  }
+
+
+
+  lookAt() {
+    const radar1 = this.scene.getObjectByName('radar-3');
+    const radar2 = this.scene.getObjectByName('radar-2');
+
+    window.radar1 = radar1;
+    window.radar2 = radar2;
+
+    const { x, y, z } = radar2.position;
+
+    radar1.lookAt(x, y, z);
+
+    // radar1.rotateZ(Math.PI / 2);
+    // radar1.rotateX(Math.PI / 2);
+  }
+
+  destroy () {
+    super.destroy();
+  }
+
+  dispose () {
+    this.destroy()
+  }
+
+}
